bf83e5422d42e0a4b5be786db297a21580f39c50
Syafa Hadyan <syafahadyanrasendriya@gmail.com>
2023-02-17 09:29:37 +0800

    Fix day

--- Schedule.py
+++ Schedule.py
@@ -6,23 +6,23 @@ Wednesday = ["Agama Islam", "PJOK", "Matematika Wajib", "Bahasa Inggris Minat"]
 Thursday = ["Fisika", "Bahasa Inggris Wajib", "Matematika Minat", "Prakarya", "Seni Budaya"]
 Friday = ["Fisika", "Bahasa Indonesia", "Biologi"]
 
-if Day == Monday:
+if Day == "Monday":
     for mon in Monday:
         print(mon)
 
-elif Day == Tuesday:
+elif Day == "Tuesday":
     for tue in Tuesday:
         print(tue)
 
-elif Day == Wednesday:
+elif Day == "Wednesday":
     for wed in Wednesday:
         print(wed)
 
-elif Day == Thursday:
+elif Day == "Thursday":
     for thu in Thursday:
         print(thu)
 
-elif Day == Friday:
+elif Day == "Friday":
     for fri in Friday:
         print(fri)
 

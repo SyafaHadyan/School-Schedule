4e591344d92a65ada97cb0d939dc2ac3d320776d
Syafa Hadyan <syafahadyanrasendriya@gmail.com>
2023-02-17 09:27:28 +0800

    if elif

--- Schedule.py
+++ Schedule.py
@@ -1,4 +1,4 @@
-Day = "Sunday"
+Day = "Monday"
 
 Monday = ["PPkn", "Bahasa Indonesia", "Matematika Wajib", "Kimia"]
 Tuesday = ["Matematika Minat", "Bahasa Inggris Minat", "Biologi", "Kimia", "Sejarah"]
@@ -9,6 +9,22 @@ Friday = ["Fisika", "Bahasa Indonesia", "Biologi"]
 if Day == Monday:
     for mon in Monday:
         print(mon)
+
+elif Day == Tuesday:
+    for tue in Tuesday:
+        print(tue)
+
+elif Day == Wednesday:
+    for wed in Wednesday:
+        print(wed)
+
+elif Day == Thursday:
+    for thu in Thursday:
+        print(thu)
+
+elif Day == Friday:
+    for fri in Friday:
+        print(fri)
 
 else:
     print("Weekend")

c34efe5dd68487c4b59507ce15fd6de3d734f3a7
Syafa Hadyan <syafahadyanrasendriya@gmail.com>
2023-02-17 09:17:02 +0800

    Add schedule variable

---
+++ Schedule.py
@@ -0,0 +1,14 @@
+Day = "Sunday"
+
+Monday = ["PPkn", "Bahasa Indonesia", "Matematika Wajib", "Kimia"]
+Tuesday = ["Matematika Minat", "Bahasa Inggris Minat", "Biologi", "Kimia", "Sejarah"]
+Wednesday = ["Agama Islam", "PJOK", "Matematika Wajib", "Bahasa Inggris Minat"]
+Thursday = ["Fisika", "Bahasa Inggris Wajib", "Matematika Minat", "Prakarya", "Seni Budaya"]
+Friday = ["Fisika", "Bahasa Indonesia", "Biologi"]
+
+if Day == Monday:
+    for mon in Monday:
+        print(mon)
+
+else:
+    print("Weekend")
